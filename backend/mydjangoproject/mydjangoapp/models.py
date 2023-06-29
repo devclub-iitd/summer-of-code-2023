cf990572c06a5e6edb21a73a780d0227af02e0ac
jdhruv1503 <jdhruv1503@gmail.com>
2023-06-29 17:24:44 +0530

    making django app

---
+++ backend/mydjangoproject/mydjangoapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    price = models.IntegerField(default=0)
+    addDate = models.DateTimeField("date added")
+    sold = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="product_images/")
+
+    def __str__(self):
+        return str(self.id)+" - "+self.name
+    
+    def isSold(self):
+        return self.sold
---
+++ backend/mydjangoproject/mydjangoapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def index(request):
+    return HttpResponse("Hello, world. This is the mydjangoapp App!")
+
