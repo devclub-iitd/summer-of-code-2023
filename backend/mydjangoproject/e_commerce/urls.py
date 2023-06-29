481f91ad5a5ed81c4f15cacff670216fce50d539
CodeGreatCommander <rohcodinG@gmail.com>
2023-06-29 23:14:36 +0530

    Part 2D

--- backend/mydjangoproject/e_commerce/urls.py
+++ backend/mydjangoproject/e_commerce/urls.py
@@ -1,3 +1,3 @@
 from django.urls import path
 from . import views
-urlpatterns=[path("",views.visit,name="visit"),]
+urlpatterns=[path("",views.visit,name="visit"),path("login",views.login_page,name="login"),]
--- backend/mydjangoproject/e_commerce/views.py
+++ backend/mydjangoproject/e_commerce/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from e_commerce.models import Product
 from django.template import loader
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_protect
 
 def visit(request):
     template = loader.get_template("visit.html")
     print(Product.objects.all())
     context ={"ls":list(Product.objects.all())}
     return HttpResponse(template.render(context, request))
+
+@csrf_protect
+def login_page(request):
+    if(request.method=='POST'):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('visit')
+        else:
+            return redirect('login')
+    else:
+        return HttpResponse(render(request,"login.html",{"failed":False}))
