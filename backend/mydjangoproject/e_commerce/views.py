14be11ce26b225c38af5271ab9b21d70834cc3d3
CodeGreatCommander <rohcodinG@gmail.com>
2023-06-30 10:10:41 +0530

    Part2D

--- backend/mydjangoproject/e_commerce/views.py
+++ backend/mydjangoproject/e_commerce/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.forms import UserCreationForm
 
 def visit(request):
     template = loader.get_template("visit.html")
@@ -24,4 +25,20 @@ def login_page(request):
         else:
             return redirect('login')
     else:
-        return HttpResponse(render(request,"login.html",{"failed":False}))
+        return HttpResponse(render(request,"login.html",{}))
+    
+@csrf_protect
+def sign_up(request):
+    if(request.method=="POST"):
+        form=UserCreationForm(request.POST)
+        if(form.is_valid()):
+            form.save()
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password1']
+            user=authenticate(username=username,password=password)
+            login(request,user)
+            return redirect('visit')
+        else:
+            return redirect('sign_up')
+    form=UserCreationForm()
+    return render(request,'signup.html',{'form':form})
