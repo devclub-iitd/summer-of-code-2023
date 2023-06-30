3e86297fae59e573372b06f6795c11117cc9e8d2
CodeGreatCommander <rohcodinG@gmail.com>
2023-06-30 18:37:35 +0530

    Bonus:Postgresql

--- backend/mydjangoproject/mydjangoproject/settings.py
+++ backend/mydjangoproject/mydjangoproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mydjangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'mydjangoproject',
+       'USER': 'postgres',
+       'PASSWORD': '',
+       'HOST': 'localhost',
+       'PORT': '5432',
     }
 }
 
