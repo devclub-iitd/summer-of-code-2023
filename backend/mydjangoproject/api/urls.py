bcc427f394e5f42434bc35a68ecaa1754aed72b5
17vali <rishiksaha2004@gmail.com>
2023-07-14 01:37:11 +0530

    week 3 bonus (swagger)

--- backend/mydjangoproject/api/urls.py
+++ backend/mydjangoproject/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -19,11 +19,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
 	openapi.Info(
-		title="Snippets API",
+		title="API Docs",
 		default_version='v1',
-		description="Test description",
+		description="Ecommerce",
 		terms_of_service="https://www.google.com/policies/terms/",
-		contact=openapi.Contact(email="contact@snippets.local"),
+		contact=openapi.Contact(email="contact@xyz.local"),
 		license=openapi.License(name="BSD License"),
 	),
 	public=True,
@@ -41,5 +41,8 @@ urlpatterns = [
     
 	path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('signup/', signupAPIView.as_view(), name="signup")
+    path('signup/', signupAPIView.as_view(), name="signup"),
+    
+	path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+	path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- backend/mydjangoproject/api/views.py
+++ backend/mydjangoproject/api/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
 from rest_framework.permissions import 	IsAuthenticated
 
