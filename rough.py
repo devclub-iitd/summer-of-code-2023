8af772d9fa829a7ab6dd73bceb0ee311faac9d1d
Pritesh Mehta <4mehtapritesh@gmail.com>
2023-07-28 19:31:57 +0530

    App submission

--- rough.py
+++
@@ -1,5 +0,0 @@
-import requests as r
-x = r.get("https://marketplace-1-b3203472.deta.app/search/")
-for y in x.json():
-    id = (y["_id"]["$oid"])
-    r.delete(f"https://marketplace-1-b3203472.deta.app/product/{id}")
