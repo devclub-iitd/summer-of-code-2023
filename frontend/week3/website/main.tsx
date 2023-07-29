d7e4b7972ed4e4c89a11543d720160978ebe8d21
jahnabiroy <137321126+jahnabiroy@users.noreply.github.com>
2023-07-29 12:25:18 +0530

    Update main.tsx

--- frontend/week3/website/main.tsx
+++ frontend/week3/website/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from 'https://jahnabiroy.github.io/summer-of-code-2023/frontend/week3/website/App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
