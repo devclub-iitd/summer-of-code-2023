37f9635841a2d019047833c90eaba762b4c7e554
jdhruv1503 <jdhruv1503@gmail.com>
2023-07-31 14:35:18 +0530

    added PostgreSQL support

--- backend/mydjangoproject/mydjangoproject/settings.py
+++ backend/mydjangoproject/mydjangoproject/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = "mydjangoproject.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "djangoproj",
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
