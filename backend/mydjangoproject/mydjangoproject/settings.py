d779f4c39e52db7ae28148e037799536faf841d1
jdhruv1503 <jdhruv1503@gmail.com>
2023-07-31 14:42:39 +0530

    fix2

--- backend/mydjangoproject/mydjangoproject/settings.py
+++ backend/mydjangoproject/mydjangoproject/settings.py
@@ -95,6 +95,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "djangoproj",
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
