409b8ea8f9767855ad54828ff255e7156b1f3b12
notagoodnaina <92837397+notagoodnaina@users.noreply.github.com>
2023-08-10 17:39:30 +0530

    Revert "Final Submission - Jahnabi"

--- frontend/week3/website/Home/LearnMore.tsx
+++
@@ -1,8 +0,0 @@
-import bg from './homeasset/comingsoon.png'
-export default function Learn() {
-    return(
-        <div style={{backgroundColor:'#033a3d'}}>
-            <img src={bg} className='d-block w-100 mx-auto' alt='..'/>
-        </div>
-    );
-}
